ee15635406fd985f0d8e0314e4c2d5820c99866d
Paulo Vanderley <vanderley.1109@gmail.com>
2024-05-31 17:46:32 -0300

    :page_with_curl: Update file content.

--- Dentista.java
+++ Dentista.java
@@ -49,6 +49,7 @@ public class Dentista {
 
     public Dentista(String nome, String sobrenome, String especialidade, String inscricaoCRO, boolean estado) {
         this.nome = nome;
+        this.sobrenome = sobrenome;
     }
 
 }
